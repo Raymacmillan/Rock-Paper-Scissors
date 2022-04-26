83b272ec019e40d8c39a792a1716965cca3d7fcf
Raymacmillan <macmillanryo@gmail.com>
2022-04-26 10:16:02 -0400

    EDIT main.js

--- main.js
+++ main.js
@@ -1,56 +1,61 @@
-function computerPlay(rock,paper,scissors){
-    let choices =[rock,paper,scissors];
-    choices = Math.floor(Math.random()* choices.length);
-    if(choices==0){
-        return("Rock");
-    }else if(choices==1){
-        return("Paper")
-    }else{
-        return("Scissors")
-    }
+const buttons = document.querySelector(".btns-container");
+const playerScore = document.querySelector(".playerScore");
+const computerScore = document.querySelector(".computerScore");
+const comment = document.querySelector(".comment");
+
+function computerPlay(){
+    let choices =["rock", "paper", "scissors"];
+    return choices[Math.floor(Math.random()* choices.length)];
 }
+
 
 computerWins = 0;
 playerWins = 0;
 
+
+let playerSelection="";
+let computerSelection = "";
+
+
+
 function playRound(playerSelection,computerSelection){
     if(computerSelection==="paper" && playerSelection==="rock"){
-       console.log("Computer wins,Paper beats rock +1");
+       comment.innerHTML = "Computer wins,Paper beats rock +1";
        computerWins+=1;
     }else if(playerSelection==="paper" && computerSelection==="scissors"){
-       console.log("Computer wins,Scissors beats paper +1");
+       comment.innerHTML = "Computer wins,Scissors beats paper +1";
        computerWins+=1;
     }else if(playerSelection ==="scissors" && computerSelection==="rock"){
-        console.log("Computer wins,Rock beats Scissors +1");
+        comment.innerHTML = "Computer wins,Rock beats Scissors +1";
         computerWins+=1;
     }else if(computerSelection==="paper" && playerSelection==="rock"){
-        console.log("Computer wins,Paper beats rock +1");
+        comment.innerHTML = "Computer wins,Paper beats rock +1";
         computerWins+=1
      }else if(computerSelection==="scissors" && playerSelection==="rock"){
-         console.log("Player wins,Rock beats Scissors +1");
+         comment.innerHTML = "Player wins,Rock beats Scissors +1";
          playerWins+=1;
      }else if(computerSelection==="rock" && playerSelection==="paper"){
-         console.log("Player wins,Paper beats Rock +1");
+         comment.innerHTML = "Player wins,Paper beats Rock +1";
          playerWins+=1;
      }else if(computerSelection==="paper" && playerSelection==="scissors"){
-         console.log("Player wins,Scissors beats paper +1");
+         comment.innerHTML = "Player wins,Scissors beats paper +1";
          playerWins+=1;
      }else{
-         console.log("Its a tie");
+         comment.innerHTML = "Its a tie";
      }
     
 }
 
-function game(){
-    for(let i =0;i<5;i++){
-        let playerSelection= prompt("Enter choice:").toLowerCase();
-        let computerSelection = computerPlay("rock","paper","scissors").toLowerCase();
-        playRound(playerSelection,computerSelection);
+
+
+
+
+buttons.addEventListener("click", ({target})=> {
+    if(target.tagName === "BUTTON"){
+       playerSelection = target.innerHTML.toLowerCase();
+       computerSelection = computerPlay();
+       playRound(playerSelection, computerSelection);
+       playerScore.innerHTML = `Player: ${playerWins}`;
+       computerScore.innerHTML = `Computer: ${computerWins}`;
     }
-    if(computerWins>playerWins){
-        console.log("Computer won by" + ""  + computerWins + "points")
-    }else{
-        console.log("Player won by" + "" + playerWins + "points")
-    }
-}
-game();
+})
